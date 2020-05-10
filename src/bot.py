6e9f6c6111b18df1657db9c9aab01901aef81ef4
Cody <codyquinn1122@gmail.com>
2020-05-10 01:59:09 -0500

    Now gets token from token.txt and added clientid.txt

--- src/bot.py
+++ src/bot.py
@@ -1,8 +1,23 @@
 import discord
 
 
+def get_token():
+    with open("token.txt", "r") as f:
+        lines = f.readlines()
+        return lines[0].strip()
+token = get_token()
+
+
+def get_client_id():
+    with open("clientid.txt", "r") as f:
+        lines = f.readlines()
+        return lines[0].strip()
+client_id = get_client_id()
+
+
 def main():
-    pass
+    client = discord.Client()
+    client.run(token)
 
 
 if __name__ == "__main__":
