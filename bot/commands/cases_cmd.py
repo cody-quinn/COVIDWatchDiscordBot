681ba87308f7d3a31dcb896644d55da93359fb18
Cody Quinn <codyquinn1122@gmail.com>
2020-05-22 22:40:23 -0500

    Added global scope for cases cmd

--- bot/commands/cases_cmd.py
+++ bot/commands/cases_cmd.py
@@ -1,5 +1,5 @@
-import discord
-from bot.commands.command import Command
+import discord, requests, datetime
+from commands.command import Command
 
 class CasesCMD(Command):
     async def run(self, message, raw_args):
@@ -12,7 +12,7 @@ class CasesCMD(Command):
             for i in arg:
                 if i == '':
                     arg.remove('')
-            if len(arg > 0):
+            if len(arg) > 0:
                 args.append(arg)
 
         for arg in args:
@@ -22,10 +22,42 @@ class CasesCMD(Command):
                 if len(arg) > 1:
                     scope = arg[1]
                 else:
-                    ##TODO: Add callback
+                    await message.channel.send("**`You must enter the name of a country you would like to scope into.`**")
             if arg[0] == "all" or arg[0] == "a":
                 allInfo = True
-            if arg[0] == "day":
-                ##TODO: Add day selector
+            # if arg[0] == "day":
+            #     ##TODO: Add date selector
 
+        req = requests.get('https://api.covid19api.com/summary')
+        if req.status_code > 199 and req.status_code < 300: #Im afraid the country you selected could not be found, maybe there was a spelling mistake?
+            results = req.json()
+            embed = discord.Embed(colour=discord.Colour(0x9c0519), timestamp=datetime.datetime.utcfromtimestamp(1589100774))
 
+            if scope.lower() == "global":
+                res_g = results['Global']
+
+                embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/International_Flag_of_Planet_Earth.svg/800px-International_Flag_of_Planet_Earth.svg.png")
+                embed.set_footer(text="Covid Watch - Coronavirus Statistics",
+                                    icon_url="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/SARS-CoV-2_without_background.png/220px-SARS-CoV-2_without_background.png")
+
+                death_rate = str(int(str(res_g['TotalDeaths']).replace(',','')) / int(str(res_g['TotalConfirmed']).replace(',','')) * 100)
+                death_rate = death_rate[:6] + "%"
+                active_cases = str(res_g['TotalConfirmed'] - res_g['TotalDeaths'] - res_g['TotalRecovered'])
+
+                embed.add_field(name="Currently Infected", value='{:,}'.format(int(active_cases)), inline=False)
+                embed.add_field(name="Total Recovered", value='{:,}'.format(int(res_g['TotalRecovered'])), inline=False)
+                embed.add_field(name="Total Deaths", value='{:,}'.format(int(res_g['TotalDeaths'])), inline=False)
+                embed.add_field(name="Total Cases", value='{:,}'.format(int(res_g['TotalConfirmed'])))
+                embed.add_field(name="Death Rate", value=death_rate)
+
+                if allInfo:
+                    embed.add_field(name="Cases Per Million", value="Coming Soon")
+                    embed.add_field(name="New Cases", value='{:,}'.format(int(res_g['NewConfirmed'])))
+                    embed.add_field(name="New Deaths", value='{:,}'.format(int(res_g['NewDeaths'])))
+                    embed.add_field(name="New Recovered", value='{:,}'.format(int(res_g['NewRecovered'])))
+            else: # https://www.countryflags.io/be/flat/64.png
+                pass #TODO: Add in scoped results
+
+            await message.channel.send(content="‌‌ \n**`Coronavirus Statistics for "+scope+"`**\n*`Last Update: "+str(results['Countries'][0]['Date'])+"`*", embed=embed)
+        else:
+            await message.channel.send("**`Covid api returned status code {}.`**".format(req.status_code))
