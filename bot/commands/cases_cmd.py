0df80460d025697ebcfd0156bab37c1ef1ec1707
Cody Quinn <codyquinn1122@gmail.com>
2020-05-28 20:03:26 -0500

    Added base for province and city selection

--- bot/commands/cases_cmd.py
+++ bot/commands/cases_cmd.py
@@ -5,7 +5,9 @@ from bot import get_data
 
 class CasesCMD(Command):
     async def run(self, message, raw_args):
-        scope = "global"
+        scope_country = "global"
+        scope_province = ""
+        scope_city = ""
         all_info = False
 
         args = []
@@ -22,17 +24,37 @@ class CasesCMD(Command):
                 continue
             if arg[0] == "country" or arg[0] == "c":
                 if len(arg) > 1:
-                    scope = ""
+                    scope_country = ""
                     for scopeArg in arg[1:]:
-                        scope = scope + str(scopeArg)
+                        scope_country = scope_country + str(scopeArg)
                         if scopeArg != arg[len(arg)-1]:
-                            scope = scope + " "
+                            scope_country = scope_country + " "
 
                     # Adding special cases to countries that should have them
-                    if scope.lower() == "usa" or scope.lower() == "united states":
-                        scope = "US"
+                    if scope_country.lower() == "usa" or scope_country.lower() == "united states":
+                        scope_country = "US"
                 else:
                     await message.channel.send("**`You must enter the name of a country you would like to scope into.`**")
+            if arg[0] == "province" or arg[0] == "state" or arg[0] == "p" or arg[0] == "s":
+                if len(arg) > 1:
+                    scope_province = ""
+                    for scopeArg in arg[1:]:
+                        scope_province = scope_province + str(scopeArg)
+                        if scopeArg != arg[len(arg) - 1]:
+                            scope_province = scope_province + " "
+                else:
+                    await message.channel.send(
+                        "**`You must enter the name of a province you would like to scope into.`**")
+            if arg[0] == "city":
+                if len(arg) > 1:
+                    scope_city = ""
+                    for scopeArg in arg[1:]:
+                        scope_city = scope_city + str(scopeArg)
+                        if scopeArg != arg[len(arg) - 1]:
+                            scope_city = scope_city + " "
+                else:
+                    await message.channel.send(
+                        "**`You must enter the name of a city you would like to scope into.`**")
             if arg[0] == "all" or arg[0] == "a":
                 all_info = True
             # if arg[0] == "day":
@@ -44,7 +66,7 @@ class CasesCMD(Command):
             embed.set_footer(text="Covid Watch - Coronavirus Statistics",
                              icon_url="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/SARS-CoV-2_without_background.png/220px-SARS-CoV-2_without_background.png")
 
-            if scope.lower() == "global":
+            if scope_country.lower() == "global":
                 res_g = data['Result']['Global']['Global']
 
                 embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/International_Flag_of_Planet_Earth.svg/800px-International_Flag_of_Planet_Earth.svg.png")
@@ -66,7 +88,7 @@ class CasesCMD(Command):
                     embed.add_field(name="New Recovered", value='{:,}'.format(int(res_g['NewRecovered'])))
             else:
                 for country in data['Result']['Global']['Countries']:
-                    if country['Country'].lower() == scope.lower() or country['CountryCode'].lower() == scope.lower() or country['Slug'] == scope.lower():
+                    if country['Country'].lower() == scope_country.lower() or country['CountryCode'].lower() == scope_country.lower() or country['Slug'] == scope_country.lower():
                         embed.set_thumbnail(url="https://www.countryflags.io/"+country['CountryCode'].lower()+"/flat/64.png")
 
                         death_rate = str(int(str(country['TotalDeaths']).replace(',','')) / int(str(country['TotalConfirmed']).replace(',','')) * 100)
@@ -85,6 +107,6 @@ class CasesCMD(Command):
                             embed.add_field(name="New Deaths", value='{:,}'.format(int(country['NewDeaths'])))
                             embed.add_field(name="New Recovered", value='{:,}'.format(int(country['NewRecovered'])))
 
-            await message.channel.send(content="‌‌ \n**`Coronavirus Statistics for "+scope+"`**\n*`Last Update: "+str(data['Result']['Global']['Countries'][0]['Date'])+"`*", embed=embed)
+            await message.channel.send(content="‌‌ \n**`Coronavirus Statistics for "+scope_country+"`**\n*`Last Update: "+str(data['Result']['Global']['Countries'][0]['Date'])+"`*", embed=embed)
         else:
             await message.channel.send("**`Covid api returned status code {}. Please wait a minute then rerun this command as these issues usually fix themselves.`**".format(data['Status']))
