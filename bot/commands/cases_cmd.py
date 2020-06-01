b7a3d87a5a4869a5f656cb5c672f7d078ef2847d
Cody Quinn <codyquinn1122@gmail.com>
2020-05-31 23:55:57 -0500

    Added check to make sure country exists inside cases command

--- bot/commands/cases_cmd.py
+++ bot/commands/cases_cmd.py
@@ -1,4 +1,4 @@
-import discord, datetime, time, calendar
+import discord, datetime
 from bot.commands.command import Command
 from bot.daemons.update_data import UpdateData
 
@@ -49,9 +49,9 @@ class CasesCMD(Command):
             try:
                 embed = discord.Embed(
                     title=title,
-                    description="Please **[vote](https://top.gg/bot/708929935443492995)** for my bot on **top.gg** | All data from **[covid19api](https://covid19api.com/)** \nStatistics last updated **{}**".format(str(self.covidData.results['Countries'][0]['Date'])),
+                    description="Please **[vote](https://top.gg/bot/708929935443492995)** for my bot on **top.gg** | All data from **[covid19api](https://covid19api.com/)** \nStatistics last updated **{} GMT**".format(str(datetime.datetime.fromisoformat(self.covidData.results['Countries'][0]['Date'][:19]))),
                     colour=discord.Colour(0x9c0519),
-                    timestamp=datetime.datetime.utcfromtimestamp(calendar.timegm(time.gmtime()))
+                    timestamp=datetime.datetime.now()
                 )
                 embed.set_footer(text="Covid Watch - Coronavirus Statistics",
                                  icon_url="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/SARS-CoV-2_without_background.png/220px-SARS-CoV-2_without_background.png")
@@ -75,7 +75,16 @@ class CasesCMD(Command):
                                 value='**{}**'.format('0'))
                 await message.channel.send(embed=embed)
             except UnboundLocalError:
-                await message.channel.send("Country {} was not found.".format(scope))
+                embed = discord.Embed(
+                    title="COVIDWatch Error",
+                    description="Were sorry however we could not find a country under **{}** inside our database. Maybe try using its full name or another alias, and make sure you spelt it correctly. You can find a list of all supported countries **[here](https://api.covid19api.com/summary)**".format(scope),
+                    colour=discord.Colour(0x9c0519),
+                    timestamp=datetime.datetime.now()
+                )
+                embed.set_footer(text="Covid Watch - Coronavirus Statistics",
+                                 icon_url="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/SARS-CoV-2_without_background.png/220px-SARS-CoV-2_without_background.png")
+
+                await message.channel.send(embed=embed)
         else:
             await message.channel.send("**`Covid api returned status code {}. Please wait a minute then rerun this command as these issues usually fix themselves.`**".format(self.covidData.unstable_req.status_code))
 
